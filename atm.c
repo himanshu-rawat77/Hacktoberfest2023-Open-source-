01b28d83c02b361be5db42b89cf85dd12d591af9
Himanshu Rawat <113584120+himanshu-rawat77@users.noreply.github.com>
2023-10-24 16:58:02 +0530

    ATM.c

--- atm.c
+++ atm.c
@@ -1,89 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<time.h>
+void delay(float number_of_seconds);
 int main()
 {
-	main:
-    float x,y;
-    char ch;
-    char choice;
-    printf("                       =======================================================");
-    printf("\n                      |                      ATM MACHINE                     |");
-    printf("\n                      ========================================================");
-    printf("\nEnter initial amount: ");
-    scanf("%f",&x);
-    printf("\n**********************ATM SERVICES*************************");
-    printf("\nEnter c for credit.\nEnter d for debit.\nEnter b for balance.\n ");
-    scanf("\n%c", &ch);
-    printf("\n************************************************************");
-    switch(ch)
+    int a, b, card_number, pin, amount, acc_number;
+    printf("Welcome the ATM of XYZ Bank ^_^\n");
+    printf("\n");
+    printf("Please Choose Service !\n");
+    printf("\n");
+    printf("1. Cash Withdrawal\n2. Balance enquiry : ");
+    scanf("%d", &a);
+    float i;
+    for(i=0; i<10; i++)
     {
-    	case 'c':
-    		printf("\n                    Enter credit amount:");
-    		scanf("%f",&y);
-    		x=x+y;
-    		printf("\n                       New Amount = %f",x);
-    		printf("\n                      PRESS b TO GO BACK:");
-    		scanf("\n%c",&choice);
-    		system("cls");
-    		if(choice=='b')
-    		{
-    			goto main;
-			}
-			else
-			{
-			  break;
-		    }
-    	case 'd':
-    		printf("\n                     Enter debit amount:");
-    		scanf("%f",&y);
-    		if(x>=y)
-    		{
-			   x=x-y;
-			   printf("\n                    New amount = %f",x);
-    	    }
-    	    else
-    	    {
-    	    	printf("\n          Insufficient amount in your account");
-			}
-    		printf("\n                      PRESS b TO GO BACK:");
-    		scanf("\n%c",&choice);
-    		system("cls");
-    		if(choice=='b')
-    		{
-    			goto main;
-			}
-			else
-			{
-			  break;
-		    }
-    	case 'b':
-    		printf("\n                Amount in your account is = %f",x);
-    	    printf("\n                       PRESS b TO GO BACK:");
-    		scanf("\n%c",&choice);
-    		system("cls");
-    		if(choice=='b')
-    		{
-    			goto main;
-			}
-			else
-			{
-			  break;
-		    }
-    	default:
-    		printf("\n                    Invalid choice.");
-    		printf("\n                  PRESS b TO GO BACK:");
-    		scanf("\n%c",&choice);
-    		system("cls");
-    		if(choice=='b')
-    		{
-    			goto main;
-			}
-			else
-			{
-			  break;
-		    }
+      delay(0.2);
+    }
 
-	}
-
+    if (a==1)
+    {
+        printf("Choose Account type\n");
+        printf("1. Domestic\n 2. Savings :  ");
+        scanf("%d", &b);
+        if (b ==1 || b==2)
+        {
+            printf("Enter Card Number : ");
+            scanf("%d", &card_number);
+            for (float j =0;  j<10; j++)
+            {
+                delay(0.2);
+            }
+            printf("Enter your pin : ");
+            scanf("%d", &pin);
+            printf("Wait for a while\n Don't PRESS any Button\n");
+            for(float k = 0; k<10; k++)
+            {
+                delay(0.2);
+            } 
+            printf("Enter the Amount(In the multiple of 100) : ");
+            scanf("%d", &amount);
+            for(float l=0; l<10; l++)
+            {
+                delay(0.5);
+            } 
+            printf("COLLECT YOUR MONEY ! \n");
+            for (float y = 0; y<10; y++)
+            {
+                delay(0.01);
+            }
+            printf("Thank you for using the ATM ^_^\n");
+        }
+    }
+    if(a==2)
+    {
+        printf("Enter Account number : ");
+        scanf("%d", &acc_number);
+        printf("Enter the ATM Pin : ");
+        scanf("%d", &pin);
+        for(float h =0; h<10; h++)
+        {
+            delay(0.5);
+        }
+        printf("Your Account Balance = ********");
+        printf("Thanks for Using ATM of XYZ Bank\n");
+    }
     return 0;
 }
+
+void delay(float number_of_seconds)
+{
+    //Converting time into milliseconds
+    float milli_seconds= 1000*number_of_seconds;
+    //Storing start time 
+    clock_t start_time = clock();
+    //looping till required time is not achieved
+    while (clock() < start_time + milli_seconds);
+}
+
